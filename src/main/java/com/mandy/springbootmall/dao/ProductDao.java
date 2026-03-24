bf661c8b34687b99cf5e0951e30f06df0d737d34
IEAD\2312004 <mandy.li@iisigroup.com>
2026-03-24 09:29:24 +0800

    商品功能新增

--- src/main/java/com/mandy/springbootmall/dao/ProductDao.java
+++ src/main/java/com/mandy/springbootmall/dao/ProductDao.java
@@ -1,7 +1,9 @@
 package com.mandy.springbootmall.dao;
 
+import com.mandy.springbootmall.dto.ProductRequest;
 import com.mandy.springbootmall.model.Product;
 
 public interface ProductDao {
     Product getProductById(Integer productId);
+    Integer createProduct(ProductRequest productRequest);
 }
--- src/main/java/com/mandy/springbootmall/service/ProductService.java
+++ src/main/java/com/mandy/springbootmall/service/ProductService.java
@@ -1,8 +1,10 @@
 package com.mandy.springbootmall.service;
 
+import com.mandy.springbootmall.dto.ProductRequest;
 import com.mandy.springbootmall.model.Product;
 
 public interface ProductService {
     Product getProductById(Integer productId);
+    Integer createProduct(ProductRequest productRequest);
 
 }
